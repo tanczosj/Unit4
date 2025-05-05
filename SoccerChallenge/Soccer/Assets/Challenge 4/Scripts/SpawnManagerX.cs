43a776e6874755206a5c180f9c24323fd2cd31f0
Joshua Tanczos <tanczosj@roverkids.org>
2025-05-05 13:42:17 -0400

    Finished Unit 4

--- SoccerChallenge/Soccer/Assets/Challenge 4/Scripts/SpawnManagerX.cs
+++ SoccerChallenge/Soccer/Assets/Challenge 4/Scripts/SpawnManagerX.cs
@@ -15,12 +15,16 @@ public class SpawnManagerX : MonoBehaviour
     public int waveCount = 1;
 
 
-    public GameObject player; 
+    public GameObject player;
+    void Start()
+    {
+        
+    }
 
     // Update is called once per frame
     void Update()
     {
-        enemyCount = GameObject.FindGameObjectsWithTag("Powerup").Length;
+        enemyCount = GameObject.FindGameObjectsWithTag("Enemy").Length;
 
         if (enemyCount == 0)
         {
@@ -49,7 +53,7 @@ public class SpawnManagerX : MonoBehaviour
         }
 
         // Spawn number of enemy balls based on wave number
-        for (int i = 0; i < 2; i++)
+        for (int i = 0; i < enemiesToSpawn; i++)
         {
             Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
         }
